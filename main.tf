14af7a1824eaa12b8fd0a88d1176f309eeb5a632
JamesLau2001 <james_lau2001@outlook.com>
2025-08-22 11:53:57 +0100

    added subnet and sg

--- main.tf
+++ main.tf
@@ -11,9 +11,26 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"]
 }
 
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.ssh_key.private_key_pem
+  filename = "./.ssh/terraform_rsa"
+}
+
+resource "local_file" "public_key" {
+  content  = tls_private_key.ssh_key.public_key_openssh
+  filename = "./.ssh/terraform_rsa.pub"
+}
+
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+  subnet_id              = "subnet-84e2f4fc"
+  vpc_security_group_ids = ["sg-00d5a1d67232f43a4"]
   key_name = "terraform-practice"
   tags = {
     Name = "terraform-practice"
